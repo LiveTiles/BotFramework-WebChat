95e0d33d3bf16c2b88f6b6c9645ac47a626d7340
Douw Loots <douw.loots@livetiles.nyc>
2017-06-19 16:03:25 +1000

    Separated HTML, bot JS and environmental variables into separate files to simplify update process. Implemented "ConversationUpdate" event on back channel.

---
+++ LiveBots/prod-core/environment.js
@@ -0,0 +1,4 @@
+var environment = {
+    botId: "bbbd2410-2556-486c-96a1-557ac00ca41d",
+    directLineSecret: "TwyDj9UIfPQ.cwA.BGU.xCR6_st78LyyL37kutTMfjw-sAwGaR2UEdju_UAojfs"
+}
---
+++ LiveBots/test-core/environment.js
@@ -0,0 +1,4 @@
+var environment = {
+    botId: "dba7a28e-c0b5-4339-a1ad-841a3fc2c46f",
+    directLineSecret: "xiAy5XscAVQ.cwA.WlI.kciHBIkyM0IXJNrX95kIAex8IyeSd8YpgpHvgH2q0Ao"
+}
