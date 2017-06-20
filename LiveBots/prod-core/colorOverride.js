33728e2bd803818a5e39c0fce33005b27c16fd52
Douw Loots <douw.loots@livetiles.nyc>
2017-06-20 14:33:03 +1000

    Added color override function

---
+++ LiveBots/prod-core/colorOverride.js
@@ -0,0 +1,23 @@
+const colorOverride = () => {
+    console.log("Overriding brand color");
+
+    var style = document.createElement("style");
+    css = ".wc-header {background-color: " + brandColor + ";} " +
+        ".wc-card button {color: " + brandColor + "; } " +   
+        ".wc-card button:hover {border-color: " + brandColor + "; color: " + brandColor + "; } " +
+        ".wc-card button:active {background-color: " + brandColor + "; border-color: " + brandColor + "; } " +
+        ".wc-console .wc-send-btn {background-color: " + brandColor + "; } " +
+        ".wc-console.has-text .wc-send svg {fill: " + brandColor + "; } " +
+        ".wc-message-from-me.wc-message-content {background-color: " + brandColor + "; } " +
+        ".wc-message-from-me svg.wc-message-callout path {fill: " + brandColor + "; } ";
+
+    if (style.styleSheet) {
+        // IE
+        style.styleSheet.cssText = css;
+    } else {
+        // Other browsers
+        style.innerHTML = css;
+    }
+        
+    document.head.appendChild(style);
+}
